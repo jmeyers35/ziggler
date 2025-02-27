d489c19222de610071170827f7e2f7b895d5471e
Jacob Meyers <jacobmeyers065@gmail.com>
2025-02-27 06:49:17 -0800

    add request parsing logic

---
+++ src/protocol.zig
@@ -0,0 +1,96 @@
+const std = @import("std");
+const mem = std.mem;
+const ascii = std.ascii;
+
+pub const Operation = enum {
+    get,
+    set,
+};
+
+// For the time being, the wire protocol will be simple:
+// <Operation> followed by whitespace-separated arguments
+// "GET <key>"
+// "SET <key> <value>"
+
+pub const RequestParseError = error{
+    InvalidOperation,
+    InvalidArguments,
+};
+
+pub const ParsedRequest = struct {
+    operation: Operation,
+    key: []const u8,
+    value: ?[]const u8,
+};
+
+const arg_delimiter = ' ';
+
+pub fn parse_request(request: []const u8) RequestParseError!ParsedRequest {
+    var it = mem.splitScalar(u8, request, ' ');
+    const op = it.first();
+    var parsed_op: Operation = undefined;
+    if (mem.eql(u8, "GET", op)) {
+        parsed_op = Operation.get;
+    } else if (mem.eql(u8, "SET", op)) {
+        parsed_op = Operation.set;
+    } else {
+        // Unrecognized operation
+        return RequestParseError.InvalidOperation;
+    }
+
+    const request_key = it.next();
+    if (request_key == null) {
+        // All requests must have a key present
+        return RequestParseError.InvalidArguments;
+    }
+
+    const maybe_value = it.next();
+    if (parsed_op == Operation.set and maybe_value == null) {
+        // SET requests must also have a value
+        return RequestParseError.InvalidArguments;
+    }
+    return .{
+        .operation = parsed_op,
+        .key = request_key.?,
+        .value = maybe_value,
+    };
+}
+
+const testing = std.testing;
+const expect = testing.expect;
+const expectEqual = testing.expectEqual;
+const expectEqualStrings = testing.expectEqualStrings;
+const expectError = testing.expectError;
+
+test "happy path, get request" {
+    const request = "GET foo";
+    const parsed = try parse_request(request);
+    try expectEqual(Operation.get, parsed.operation);
+    try expectEqualStrings("foo", parsed.key);
+    try expect(parsed.value == null);
+}
+
+test "happy path, set request" {
+    const request = "SET foo bar";
+    const parsed = try parse_request(request);
+    try expectEqual(Operation.set, parsed.operation);
+    try expectEqualStrings("foo", parsed.key);
+    try expect(parsed.value != null);
+    try expectEqualStrings("bar", parsed.value.?);
+}
+
+test "unrecognized operation" {
+    try expectError(RequestParseError.InvalidOperation, parse_request("FOO bar"));
+}
+
+test "get request, no key" {
+    try expectError(RequestParseError.InvalidArguments, parse_request("GET"));
+}
+
+test "set request, no key" {
+    try expectError(RequestParseError.InvalidArguments, parse_request("SET"));
+}
+
+test "set request, no value" {
+    try expectError(RequestParseError.InvalidArguments, parse_request("SET foo"));
+}
--- src/test_main.zig
+++ src/test_main.zig
@@ -2,4 +2,5 @@ test {
     _ = @import("io.zig");
     _ = @import("server.zig");
     _ = @import("kv.zig");
+    _ = @import("protocol.zig");
 }
