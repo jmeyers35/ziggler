9f89f610c816542f3aa722e1b307db0e3f7559fd
Jacob Meyers <jacobmeyers065@gmail.com>
2025-02-27 21:06:17 -0800

    better responses; go full redis

--- src/server.zig
+++ src/server.zig
@@ -1,8 +1,14 @@
 const std = @import("std");
+const fmt = std.fmt;
 const log = std.log;
+const mem = std.mem;
+
 const assert = std.debug.assert;
+
 const protocol = @import("protocol.zig");
 const Operation = protocol.Operation;
+
+const MAX_VAL_LENGTH = 1024;
 
 // TODO: pass address in here and let server configure the IO?
 pub fn ServerType(comptime IOType: type, comptime StorageType: type) type {
@@ -45,26 +51,39 @@ pub fn ServerType(comptime IOType: type, comptime StorageType: type) type {
 
         fn handle_request(server: *Server, request: []const u8) !void {
             const parsed = protocol.parse_request(request) catch |err| {
-                try server.io.send("Parse Error");
+                // TODO: do better than this
+                try server.io.send("Parse Error\r\n");
                 log.err("error parsing request: {any}", .{err});
                 return;
             };
-
             if (parsed.operation == Operation.get) {
                 const got = server.storage.get(parsed.key) orelse "<null>";
-                log.info("got value {s} for key {s}", .{ got, parsed.key });
-                // TODO: gracefully handle nulls? how should we handle not found? check what Redis does
-                try server.io.send(got);
+                log.info("got value {s}(len:{d}) for key {s}", .{ got, got.len, parsed.key });
+                // TODO: figure out constraints around value sizes? we probably
+                // can't get away with static allocations here forever (or can we?)
+                // for now: we'll assert values are 1K or smaller
+                // maybe we'll go after very small keys and values, predictable performance
+                assert(got.len <= MAX_VAL_LENGTH);
+                var buf: [1026]u8 = undefined;
+                mem.copyForwards(u8, buf[0..got.len], got);
+                buf[got.len] = '\r';
+                buf[got.len + 1] = '\n';
+                try server.io.send(buf[0 .. got.len + 2]);
             } else if (parsed.operation == Operation.set) {
                 assert(parsed.value != null);
                 const val = parsed.value.?;
+                if (val.len > MAX_VAL_LENGTH) {
+                    try server.io.send("Error\r\n");
+                    log.err("value too large: {d}", .{val.len});
+                    return;
+                }
                 server.storage.set(parsed.key, val) catch |err| {
-                    try server.io.send("Error"); // TODO: define errors in protocol
+                    try server.io.send("Error\r\n"); // TODO: define errors in protocol
                     log.err("error writing to storage: {any}", .{err});
                     return;
                 };
                 log.info("set key {s} to {s}", .{ parsed.key, val });
-                try server.io.send("OK");
+                try server.io.send("OK\r\n");
             }
         }
     };
